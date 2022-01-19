43f7f2f819f225c73354fc4215440b23fe86baec
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-20 00:00:30 +0900

    feat: add news ch

--- text_ch.tf
+++ text_ch.tf
@@ -10,3 +10,8 @@ resource "discord_text_channel" "rule" {
   category  = discord_category_channel.announce.id
 }
 
+resource "discord_text_channel" "news" {
+  name      = "news"
+  server_id = discord_server.server.id
+  category  = discord_category_channel.announce.id
+}
