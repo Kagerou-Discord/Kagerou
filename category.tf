2d1a45f09346ca3de5aa42c0eae2c45104aac6d6
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-19 23:58:46 +0900

    fix: アナウンスカテゴリの位置を1に

--- category.tf
+++ category.tf
@@ -7,5 +7,5 @@ resource "discord_category_channel" "general" {
 resource "discord_category_channel" "announce" {
   name      = "announcement"
   server_id = discord_server.server.id
-  position  = 0
+  position  = 1
 }
