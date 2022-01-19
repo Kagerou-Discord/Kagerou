09a996826d7a57e4433e75570082af42a42cfec0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-19 10:49:57 +0900

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     discord = {
-      source = "aequasi/discord"
+      source  = "aequasi/discord"
       version = "0.0.4"
     }
   }
@@ -9,29 +9,29 @@ terraform {
 
 variable "DISCORD_TOKEN" {}
 
-provider discord {
-    token = var.DISCORD_TOKEN
+provider "discord" {
+  token = var.DISCORD_TOKEN
 }
 
-resource discord_server etoile {
-    name = "Kagerou"
-    region = "japan"
-    default_message_notifications = 1
+resource "discord_server" "etoile" {
+  name                          = "Kagerou"
+  region                        = "japan"
+  default_message_notifications = 1
 }
 
-resource discord_category_channel general {
-  name = "general"
+resource "discord_category_channel" "general" {
+  name      = "general"
   server_id = discord_server.etoile.id
-  position = 0
+  position  = 0
 }
 
-resource discord_text_channel general {
-  name = "general"
+resource "discord_text_channel" "general" {
+  name      = "general"
   server_id = discord_server.etoile.id
-  category = discord_category_channel.general.id
+  category  = discord_category_channel.general.id
 }
 
-resource discord_invite invite_to_general {
+resource "discord_invite" "invite_to_general" {
   channel_id = discord_text_channel.general.id
-  max_age = 0
+  max_age    = 0
 }
