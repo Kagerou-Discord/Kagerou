464a3312ba594cb757dc643b5fc02555efc0f978
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-20 11:25:36 +0900

    fix: category position
    announceが上

--- category.tf
+++ category.tf
@@ -1,11 +1,11 @@
 resource "discord_category_channel" "general" {
   name      = "general"
   server_id = discord_server.server.id
-  position  = 0
+  position  = 1
 }
 
 resource "discord_category_channel" "announce" {
   name      = "announcement"
   server_id = discord_server.server.id
-  position  = 1
+  position  = 0
 }
