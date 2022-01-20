566020b52930f6f120e6edbfb97481e328ca21b6
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-20 14:51:11 +0900

    feat: ruleをarrayでfor_eachしてみる

--- message.tf
+++ message.tf
@@ -1,4 +1,13 @@
+locals {
+  rules = [
+    "これは",
+    "テスト用の",
+    "ルールだよ"
+  ]
+}
+
 resource "discord_message" "hello_world" {
   channel_id = discord_text_channel.rule.id
-  content    = "This is a test message!"
+  for_each   = toset(locals.rules)
+  content    = each.value
 }
