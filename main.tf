fa67b418e91adeabb6e3d17d861464e094791165
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-20 10:47:08 +0900

    ci: fix position of cloud section

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-cloud {
-  organization = "lucky3028"
-
-  workspaces = {
-    name = "Kagerou"
-  }
-}
-
 terraform {
   required_providers {
     discord = {
       source  = "aequasi/discord"
       version = "0.0.4"
+    }
+  }
+
+  cloud {
+    organization = "lucky3028"
+
+    workspaces = {
+      name = "Kagerou"
     }
   }
 }
