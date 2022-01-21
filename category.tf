941ec99fd509021de0c672ffdc59f8085fc4851e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-22 00:15:14 +0900

    style: fix order cats

--- category.tf
+++ category.tf
@@ -1,23 +1,23 @@
-resource "discord_category_channel" "general" {
-  name      = "general"
-  server_id = discord_server.server.id
-  position  = 2
-}
-
 resource "discord_category_channel" "announce" {
   name      = "announcement"
   server_id = discord_server.server.id
   position  = 0
 }
 
-resource "discord_category_channel" "management" {
-  name      = "management"
+resource "discord_category_channel" "general" {
+  name      = "general"
   server_id = discord_server.server.id
-  position  = 3
+  position  = 2
 }
 
 resource "discord_category_channel" "read_only" {
   name      = "read-only"
   server_id = discord_server.server.id
   position  = 1
+}
+
+resource "discord_category_channel" "management" {
+  name      = "management"
+  server_id = discord_server.server.id
+  position  = 3
 }
