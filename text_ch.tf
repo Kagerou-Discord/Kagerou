29b6919449ee02aaf65af98fa9f671b066085816
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-21 10:51:51 +0900

    feat: add stock chs

--- text_ch.tf
+++ text_ch.tf
@@ -28,6 +28,19 @@ resource "discord_text_channel" "system" {
   category  = discord_category_channel.read_only.id
 }
 
+resource "discord_text_channel" "stock" {
+  name      = "stock"
+  server_id = discord_server.server.id
+  category  = discord_category_channel.read_only.id
+}
+
+resource "discord_text_channel" "stock_r18" {
+  name      = "stock-r18"
+  server_id = discord_server.server.id
+  category  = discord_category_channel.read_only.id
+  nsfw      = true
+}
+
 resource "discord_text_channel" "admin" {
   name      = "admin"
   server_id = discord_server.server.id
