ff53410ba0463987458c49a77950d6b1c3a2a5eb
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-21 11:20:17 +0900

    feat: add oshi ch

--- text_ch.tf
+++ text_ch.tf
@@ -4,6 +4,12 @@ resource "discord_text_channel" "general" {
   category  = discord_category_channel.general.id
 }
 
+resource "discord_text_channel" "oshi" {
+  name      = "推し"
+  server_id = discord_server.server.id
+  category  = discord_category_channel.general.id
+}
+
 resource "discord_text_channel" "community-update" {
   name      = "community-update"
   server_id = discord_server.server.id
