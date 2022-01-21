fcdf5714ad390eca3441a86e428a460e815e6b7c
Lucky <lucky3028@users.noreply.github.com>
2022-01-21 15:36:18 +0900

    fix: order position

--- role.tf
+++ role.tf
@@ -68,7 +68,7 @@ resource "discord_role" "member" {
   name        = "メンバー"
   server_id   = discord_server.server.id
   permissions = data.discord_permission.read_and_write.allow_bits
-  position    = 2
+  position    = 0
 }
 
 data "discord_permission" "admin" {
@@ -79,7 +79,7 @@ resource "discord_role" "admin" {
   name        = "管理用ロール"
   server_id   = discord_server.server.id
   permissions = data.discord_permission.admin.allow_bits
-  position    = 0
+  position    = 2
 }
 
 locals {
