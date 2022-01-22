905f889a809556b1a3a8a731306d52d001193708
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-22 17:10:17 +0900

    feat: add about ch
    各チャンネルの説明チャンネル

--- text_ch.tf
+++ text_ch.tf
@@ -10,6 +10,12 @@ resource "discord_text_channel" "news" {
   category  = discord_category_channel.announce.id
 }
 
+resource "discord_text_channel" "about" {
+  name      = "about"
+  server_id = discord_server.server.id
+  category  = discord_category_channel.announce.id
+}
+
 resource "discord_text_channel" "general" {
   name      = "general"
   server_id = discord_server.server.id
