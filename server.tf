632b54245ef8dbcfad04e021123c899ff31c6eec
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-22 23:55:22 +0900

    chore: remove verification level
    4を指定したいが、0から3の間にしないと怒られてしまうので、わざと指定しない

--- server.tf
+++ server.tf
@@ -4,6 +4,5 @@ resource "discord_server" "server" {
   owner_id                      = "933006368443604992"
   default_message_notifications = 1
   explicit_content_filter       = 2
-  verification_level            = 3
   system_channel_id             = discord_text_channel.system.id
 }
