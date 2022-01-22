f6103586cefc108cbe2bca087624cffbd3f241bd
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-22 23:54:31 +0900

    docs: remove docs

--- server.tf
+++ server.tf
@@ -5,7 +5,5 @@ resource "discord_server" "server" {
   default_message_notifications = 1
   explicit_content_filter       = 2
   verification_level            = 3
-  # 循環参照を引き起こしてしまうためベタ書き
-  # discord_text_channel.system
-  system_channel_id = discord_text_channel.system.id
+  system_channel_id             = discord_text_channel.system.id
 }
