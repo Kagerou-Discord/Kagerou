c4b1b6f7aafb915f47bdc9747d56d8eb58a3886b
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-01 16:13:52 +0900

    chore: remove ch for test

--- text_ch.tf
+++ text_ch.tf
@@ -5,13 +5,6 @@ resource "discord_text_channel" "newcomer" {
   sync_perms_with_category = false
 }
 
-resource "discord_text_channel" "newcomer2" {
-  name                     = "for-newcomers2"
-  server_id                = local.server_id
-  topic                    = "最初にサーバーに参加"
-  sync_perms_with_category = false
-}
-
 resource "discord_text_channel" "rule" {
   name      = "rule"
   server_id = local.server_id
