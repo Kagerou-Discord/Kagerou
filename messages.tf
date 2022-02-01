ae051239ef6ac96745e0dc0ba2bf798a68b82371
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-01 16:24:38 +0900

    style: use heredocument

--- messages.tf
+++ messages.tf
@@ -1,5 +1,8 @@
 resource "discord_message" "about" {
   channel_id = discord_text_channel.about.id
-  content    = "本Discordサーバーの各チャンネルの説明については、以下のURLをご覧ください。\nhttps://github.com/Kagerou-Discord/docs/blob/main/channels.md"
+  content    = <<-EOT
+    本Discordサーバーの各チャンネルの説明については、各チャンネルのトピックまたは以下のURLをご覧ください。
+    https://github.com/Kagerou-Discord/docs/blob/main/channels.md"
+  EOT
   pinned     = true
 }
