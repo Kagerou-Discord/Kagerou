f6cbaa39465670ea9a527f531989d57d6204962e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-01 16:34:14 +0900

    feat: add changes history

--- messages.tf
+++ messages.tf
@@ -3,6 +3,8 @@ resource "discord_message" "about" {
   content    = <<-EOT
     本Discordサーバーの各チャンネルの説明については、各チャンネルのトピックまたは以下のURLをご覧ください。
     https://github.com/Kagerou-Discord/docs/blob/main/channels.md"
+    変更履歴はこちら。
+    https://github.com/Kagerou-Discord/docs/commits/main/channels.md
   EOT
   pinned     = true
 }
