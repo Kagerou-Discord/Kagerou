a16f001fa34901f27059703f288aef1304d7222f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-01 16:52:44 +0900

    style: remove quote

--- messages.tf
+++ messages.tf
@@ -14,7 +14,7 @@ resource "discord_message" "about" {
   channel_id = discord_text_channel.about.id
   content    = <<-EOT
     本Discordサーバーの各チャンネルの説明については、各チャンネルのトピックまたは以下のURLをご覧ください。
-    https://github.com/Kagerou-Discord/docs/blob/main/channels.md"
+    https://github.com/Kagerou-Discord/docs/blob/main/channels.md
 
     変更履歴はこちら。
     https://github.com/Kagerou-Discord/docs/commits/main/channels.md
