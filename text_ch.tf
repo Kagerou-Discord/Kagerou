5045b652ea54ae8c016ead45bf938612b8fe6c34
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-03 18:59:26 +0900

    style: fix indent

--- text_ch.tf
+++ text_ch.tf
@@ -6,8 +6,8 @@ resource "discord_text_channel" "newcomer" {
 }
 
 resource "discord_news_channel" "newcomer2" {
-  name                     = "for-newcomers"
-  server_id                = local.server_id
+  name      = "for-newcomers"
+  server_id = local.server_id
 }
 
 resource "discord_text_channel" "rule" {
