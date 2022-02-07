b74188e7c39b761e19ba2d24177512e2954067bf
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-07 20:32:19 +0900

    fix: memberロールを付与する対象を定義

--- role.tf
+++ role.tf
@@ -155,6 +155,10 @@ locals {
     "255951609174032385",
     "933006368443604992"
   ]
+
+  members = [
+    "889808240454561853"
+  ]
 }
 
 resource "discord_member_roles" "admin" {
@@ -163,5 +167,14 @@ resource "discord_member_roles" "admin" {
   server_id = local.server_id
   role {
     role_id = discord_role.admin.id
+  }
+}
+
+resource "discord_member_roles" "member" {
+  for_each  = toset(local.members)
+  user_id   = each.value
+  server_id = local.server_id
+  role {
+    role_id = discord_role.member.id
   }
 }
