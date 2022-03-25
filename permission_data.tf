b1e058fc961c4b5ea47ab1495c7dac8003b477e1
Lucky <Lucky3028@users.noreply.github.com>
2022-03-25 21:11:06 +0900

    feat(perms): add change_nickname permission

--- permission_data.tf
+++ permission_data.tf
@@ -7,6 +7,7 @@ data "discord_permission" "read_only" {
   send_messages        = "deny"
   read_message_history = "allow"
   add_reactions        = "allow"
+  change_nickname      = "true"
 }
 
 data "discord_permission" "read_and_write" {
